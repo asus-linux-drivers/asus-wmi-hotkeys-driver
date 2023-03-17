1e5d2bf0a1835360ae6cf68e690b5dadee5cc011
Lukáš Drahník <ldrahnik@gmail.com>
2023-03-18 00:21:38 +0100

    #5 used instead of composited keys original keys as examples + MIC_MUTE is called properly so has been removed

--- keys_wmi_layouts/up5401ea.py
+++ keys_wmi_layouts/up5401ea.py
@@ -7,16 +7,12 @@ KEY_WMI_MYASUS = 0x86 # 134
 
 key_wmi_touchpad = [
     KEY_WMI_TOUCHPAD,
-    EV_KEY.KEY_LEFTSHIFT,
-    EV_KEY.KEY_LEFTMETA,
-    EV_KEY.KEY_N,
+    EV_KEY.KEY_TOUCHPAD_TOGGLE
 ]
 
 key_wmi_camera = [
     KEY_WMI_CAMERA,
-    EV_KEY.KEY_LEFTSHIFT,
-    EV_KEY.KEY_LEFTMETA,
-    EV_KEY.KEY_R
+    EV_KEY.KEY_CAMERA
 ]
 
 key_wmi_myasus = [
@@ -26,16 +22,8 @@ key_wmi_myasus = [
     EV_KEY.KEY_T,
 ]
 
-key_wmi_micmute = [
-    KEY_WMI_MICMUTE,
-    EV_KEY.KEY_LEFTSHIFT,
-    EV_KEY.KEY_LEFTMETA,
-    EV_KEY.KEY_Q,
-]
-
 keys_wmi = [
     key_wmi_camera,
     key_wmi_myasus,
-    key_wmi_micmute,
     key_wmi_touchpad
 ]
